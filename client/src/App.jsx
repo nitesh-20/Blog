2f4de9ff49db1588eae2ee3ff1f2ebbea72a397c
nitesh-20 <nsahu6156@gmail.com>
2024-09-16 20:53:00 +0530

    Setup Client-Site

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createBrowserRouter,RouterProvider,Route} from "react-router-dom"
+const App = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
