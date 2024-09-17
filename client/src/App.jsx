f84ab98cf5d214cd3b1ce6a0a8bf91b18d20d238
nitesh-20 <nsahu6156@gmail.com>
2024-09-17 22:09:32 +0530

    Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
-import {createBrowserRouter,RouterProvider,Route} from "react-router-dom"
-const App = () => {
+import {createBrowserRouter,RouterProvider,Route, Outlet} from "react-router-dom"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Write from "./pages/Write"
+import Home from "./pages/Home"
+import Single from "./pages/Single"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+const Layout = ()=>{
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
   )
 }
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/post/:id",
+        element: <Single/>
+      },
+      {
+        path:"/write",
+        element: <Write/>
+      },
+    ]
+  },
+  {
+    path:"/register",
+    element: <Register/> 
+  },
+  {
+    path:"/login",
+    element: <Login/> 
+  },
+])
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='container'>
+      <RouterProvider router={router}/>
+      </div>
+    </div>
+  )
+}
 export default App
