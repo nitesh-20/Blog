f6fb26f9ba7a8e97269f581b9b9698c5521315c5
nitesh-20 <nsahu6156@gmail.com>
2024-10-22 21:25:45 +0530

    Menu.jsx

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -32,8 +32,8 @@ const Menu = () => {
     <h1>Other posts you may like</h1>
     {posts.map(post=>(
         <div className="post" key={post.id}>
-            <img src="" alt="" />
-            <h2></h2>
+            <img src={post.img} alt="" />
+            <h2>{post.title}</h2>
             <button>Read More</button>
         </div>
     ))}
