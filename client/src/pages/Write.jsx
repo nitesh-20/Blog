8f9641e7cf48584bc310de7e4f5e873e6b954d24
nitesh-20 <nsahu6156@gmail.com>
2024-10-23 17:20:44 +0530

    fix error

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'quill/dist/quill.snow.css';
 const Write = () => {
+  const [value, setValue] = useState('');
+
+  console.log(value);
+
   return (
-    <div>Write</div>
+    <div className='add'>
+      <div className="content">
+        <input type="text" placeholder='title' />
+        <div className="editorContainer">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
   )
 }
 
